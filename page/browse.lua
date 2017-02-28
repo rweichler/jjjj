3adaaab9defca5a8179bb9bf8c21591b328f6493
Reed Weichler <rweichler@gmail.com>
2017-02-27 23:23:25 -0800

    whoops

--- page/browse.lua
+++ page/browse.lua
@@ -4,7 +4,7 @@ _G.BROWSECONTROLLER = objc.UINavigationController:alloc():initWithRootViewContro
     m:view():setBackgroundColor(objc.UIColor:whiteColor())
 
     local label = objc.UILabel:alloc():initWithFrame{{20, 20 + NAVHEIGHT()},{SCREEN.WIDTH - 40, 100}}
-    label:setText('TODO: put an integrated /r/jailbreak browser here... or something\n\nbtw, the source code to this app is in /var/lua/jjjj.app. If you want to add new repos edit /var/lua/jjjj.app/page/repos.lua\n\ngithub repo is github.com/rweichler/jjjj')
+    label:setText('TODO: put an integrated /r/jailbreak browser here... or something\n\nbtw, the source code to this app is in /var/lua/jjjj.app. If you want to add new repos edit /var/lua/jjjj.app/page/repos.lua\n\ngithub.com/rweichler/jjjj')
     label:setNumberOfLines(0)
     label:sizeToFit()
     m:view():addSubview(label)
